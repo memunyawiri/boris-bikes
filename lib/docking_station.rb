64568b22ca8a443764929e10d99b1d6e88d360b8
paulmillen <pwmillen@gmail.com>
2017-07-04 15:34:05 +0100

    docking_station.rb syntax error update

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,16 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
- def release_bike
-   
- end
 
- def dock(bike)
-   @bike = bike
- end
- end
+  def release_bike
+    fail "No bikes available" unless @bike
+    @bike
+  end
 
-docking_station = DockingStation.new
+  def dock(bike)
+    @bike = bike
+  end
+
+
+
+end
